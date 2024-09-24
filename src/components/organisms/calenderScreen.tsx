c4569e9719d67318a46839d39cbe9c99ae730be3
catalystcodes <muh4mub@gmail.com>
2024-09-24 15:28:49 +0100

    feat: calender

--- src/components/organisms/calenderScreen.tsx
+++ src/components/organisms/calenderScreen.tsx
@@ -1,12 +1,64 @@
 import React, { useState } from "react";
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig,
+} from "react-native-calendars";
 import { StyleSheet, View } from "react-native";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+
+LocaleConfig.locales["fr"] = {
+  monthNames: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  monthNamesShort: [
+    "Jan.",
+    "Feb.",
+    "Mar.",
+    "April.",
+    "May.",
+    "Jun.",
+    "July.",
+    "Aug.",
+    "Sept.",
+    "Oct.",
+    "Nov.",
+    "Dec.",
+  ],
+  dayNames: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+  dayNamesShort: ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
+  today: "'Today",
+};
+
+LocaleConfig.defaultLocale = "fr";
 
 const CalenderScreen = () => {
   const [selected, setSelected] = useState("");
 
   return (
     <Calendar
+      headerStyle={styles.headerStyle}
+      hideExtraDays
       onDayPress={(day) => {
         setSelected(day.dateString);
       }}
@@ -14,13 +66,29 @@ const CalenderScreen = () => {
         [selected]: {
           selected: true,
           disableTouchEvent: true,
-          selectedDotColor: "orange",
         },
+      }}
+      current={"2021-02-17"}
+      style={styles.calenderStyle}
+      theme={{
+        backgroundColor: "#EDEDED",
+        textSectionTitleColor: "#b6c1cd",
+        selectedDayBackgroundColor: "#0EBE7F",
+        dayTextColor: "#2d4150",
       }}
     />
   );
 };
 
-const styles = StyleSheet.create({});
-
 export default CalenderScreen;
+
+const styles = StyleSheet.create({
+  calenderStyle: {
+    height: hp(34.5),
+    borderRadius: 9,
+  },
+  headerStyle: {
+    // backgroundColor: "#0EBE7F",
+    // color: "white",
+  },
+});
--- src/screens/appointmentSchedule.tsx
+++ src/screens/appointmentSchedule.tsx
@@ -2,12 +2,15 @@ import React, { ReactNode, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import HeadLine from "../components/molecules/headeLine";
 import CalendarScreen from "../components/organisms/calenderScreen";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 
 const AppointmentSchedule = () => {
   return (
     <View>
       <HeadLine title="Appointment" />
-      <CalendarScreen />
+      <View style={{ paddingHorizontal: wp(5.3) }}>
+        <CalendarScreen />
+      </View>
     </View>
   );
 };
